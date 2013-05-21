1208f0bcf2358534e6aa5a7778be574ad537fc10
btorch <btorch@gmail.com>
2013-05-21 21:20:33 +0000

    moving imports to the top

--- test/unit/common/middleware/test_recon.py
+++ test/unit/common/middleware/test_recon.py
@@ -20,8 +20,10 @@ from posix import stat_result, statvfs_result
 import os
 
 import swift.common.constraints
+from swift import __version__ as swiftver
 from swift.common.swob import Request
 from swift.common.middleware import recon
+from swift.common.utils import json
 
 
 class FakeApp(object):
@@ -653,8 +655,6 @@ class TestReconMiddleware(unittest.TestCase):
         self.assertEquals(resp, get_mem_resp)
 
     def test_recon_get_version(self):
-        from swift import __version__ as swiftver
-        from swift.common.utils import json
         req = Request.blank('/recon/version', environ={'REQUEST_METHOD': 'GET'})
         resp = self.app(req.environ, start_response)
         self.assertEquals(resp, [json.dumps({'version': swiftver})])
